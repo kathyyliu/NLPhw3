7db95c9ad654915fa2e2a174af60017b03ca1799
kathyyliu <kliu4@oxy.edu>
2021-10-27 15:47:28 -0700

    generation using replace with top similar word

--- generate.py
+++ generate.py
@@ -1,3 +1,4 @@
+import nltk
 from nltk import pos_tag
 from nltk.lm.preprocessing import padded_everygram_pipeline
 from nltk.lm import MLE
@@ -16,7 +17,7 @@ def open_json(genre):
 def pos_tagging(data):
   pos_tags = {}
   for line in data:
-      for pair in pos_tag(line):
+      for pair in pos_tag(line, tagset='universal'):
           if pair[1] in pos_tags:
             pos_tags[pair[1]].add(pair[0])
           else:
@@ -30,7 +31,7 @@ def embedding(data):
         sentences=data,
         vector_size=150,
         window=4,
-        min_count=2,
+        min_count=0,
         negative=10,
         workers=cores-1,
         sg=1)
@@ -48,29 +49,42 @@ def ngrams(data, n):
     return model
 
 
-def generate(model):
-    detokenize = TreebankWordDetokenizer().detokenize
-    content = []
-    for i in range(4):
-        line = model.generate(8, text_seed=['<s>'])
-        for token in line:
-            if token == '</s>':
-                content.append('\n')
+def generate(data, n=2, num_lines=4, max_line_len=8, similarity_threshold=.998):
+    model = ngrams(data, n)
+    song_tags = pos_tagging(data)
+    vectors = embedding(data)
+    new_song = []
+    for i in range(num_lines):
+        line = model.generate(max_line_len, text_seed=['<s>'])
+        new_tags = pos_tag(line, tagset='universal')
+        for pair in new_tags:
+            new_token = pair[0]
+            new_tag = pair[1]
+            if new_token == '</s>':
                 break
-            content.append(token)
-    return detokenize(content)
+            elif new_tag in ('ADJ', 'ADV', 'NOUN', 'PROPN', 'VERB'):
+                most_similar = ('', 0)
+                for token in song_tags[new_tag]:
+                    similarity = vectors.similarity(new_token, token)
+                    if similarity > most_similar[1] and most_similar[0] != new_token:
+                        most_similar = (new_token, similarity)
+                if most_similar[0]:
+                    new_song.append(most_similar[0])
+                    continue
+            new_song.append(new_token)
+        new_song.append('\n')
+    detokenize = TreebankWordDetokenizer().detokenize
+    return detokenize(new_song)
 
 
 def main():
     # text = [['<s>', 'a', 'b', 'c', '<\s>'], ['<s>', 'a', 'c', 'd', 'c', 'e', 'f', '<\s>']]
-    # model = ngrams(text, 2)
-    # print(generate(model))
-    # print(embedding(text).most_similar(positive=['c']))
 
     data = open_json('country')
-    print(pos_tagging(data))
-
+    # print(pos_tagging(data))
+    print(generate(data))
 
 
 if __name__ == '__main__':
+    # nltk.download('universal_tagset')
     main()
