582418539e9949deb3bc4ab93652f08a9ca950d2
kathyyliu <kliu4@oxy.edu>
2021-10-26 22:22:58 -0700

    Fix generation, Add word2vec

--- generate.py
+++ generate.py
@@ -1,23 +1,29 @@
 from nltk.lm.preprocessing import padded_everygram_pipeline
 from nltk.lm import MLE
 from nltk.tokenize.treebank import TreebankWordDetokenizer
-import gensim
+from gensim.models import Word2Vec
+import multiprocessing
 
 
 def embedding(data):
-    model = gensim.models.Word2Vec(
-        data,
-        size=150,
-        window=10,
+    cores = multiprocessing.cpu_count()
+    model = Word2Vec(
+        sentences=data,
+        vector_size=150,
+        window=4,
         min_count=2,
-        workers=10,
-        iter=10)
+        negative=10,
+        workers=cores-1,
+        sg=1)
+    # model.train(
+    #     corpus_iterable=data,
+    #     total_examples=len(data),
+    #     epochs=20)
+    return model.wv     # return keyed vectors
 
 
 def ngrams(data, n):
     train, vocab = padded_everygram_pipeline(n, data)
-    print(train)
-    print(list(vocab))
     model = MLE(n)
     model.fit(train, vocab)
     return model
@@ -27,18 +33,22 @@ def generate(model):
     detokenize = TreebankWordDetokenizer().detokenize
     content = []
     for i in range(4):
-        for token in model.generate(6, text_seed=['<s>'], random_seed=3):
+        line = model.generate(8, text_seed=['<s>'])
+        for token in line:
             if token == '</s>':
+                content.append('\n')
                 break
             content.append(token)
-        content.append('\n')
     return detokenize(content)
 
 
 def main():
-    text = [['a', 'b', 'c'], ['a', 'c', 'd', 'c', 'e', 'f']]
-    model = ngrams(text, 2)
-    print('generate:', generate(model))
+    # test
+    text = [['<s>', 'a', 'b', 'c', '<\s>'], ['<s>', 'a', 'c', 'd', 'c', 'e', 'f', '<\s>']]
+    # model = ngrams(text, 2)
+    # print(generate(model))
+
+    print(embedding(text).most_similar(positive=['c']))
 
 
 if __name__ == '__main__':
